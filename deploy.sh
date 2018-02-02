56636b8e0fa53849e98a007f7b7ba025e4dde57a
Travis CI <travis@travis.org>
2018-02-02 06:08:47 +0000

    Deploy to Github Pages

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -o errexit
+
+git init
+
+git config --global user.email "travis@travis.org"
+git config --global user.name "Travis CI"
+
+git add .
+git commit -m "Deploy to Github Pages"
+git push --force --quiet "https://${GITHUB_TOKEN}@github.com/dtothefp/hypto-crypto.git" master:gh-pages > /dev/null 2>&1
